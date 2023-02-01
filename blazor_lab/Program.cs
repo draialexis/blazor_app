db92b285c6f6e6a1c5dd9eaa61714ee48a73e9d8
Alexis DRAI <alexis.drai@etu.uca.fr>
2023-02-01 17:31:36 +0100

    :construction: Get started on i18n
    https://codefirst.iut.uca.fr/documentation/julien.riboulet/docusaurus/Blazor/globalisation-localisation/translate-site-resources/#location-files

--- blazor_lab/Program.cs
+++ blazor_lab/Program.cs
@@ -5,6 +5,9 @@ using Blazorise.Icons.FontAwesome;
 using Blazored.LocalStorage;
 using blazor_lab.Services;
 using Blazored.Modal;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,6 +28,23 @@ builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped<IDataService, DataLocalService>();
 builder.Services.AddBlazoredModal();
 
+// Add the controller of the app
+builder.Services.AddControllers();
+
+// Add the localization to the app and specify the resources path
+builder.Services.AddLocalization(opts => { opts.ResourcesPath = "Resources"; });
+
+// Configure the localtization
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    // Set the default culture of the web site
+    options.DefaultRequestCulture = new RequestCulture(new CultureInfo("en-US"));
+
+    // Declare the supported culture
+    options.SupportedCultures = new List<CultureInfo> { new CultureInfo("en-US"), new CultureInfo("fr-FR") };
+    options.SupportedUICultures = new List<CultureInfo> { new CultureInfo("en-US"), new CultureInfo("fr-FR") };
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -40,6 +60,23 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+// Get the current localization options
+var options = ((IApplicationBuilder)app)
+    .ApplicationServices
+    .GetService<IOptions<RequestLocalizationOptions>>();
+
+if (options?.Value != null)
+{
+    // use the default localization
+    app.UseRequestLocalization(options.Value);
+}
+
+// Add the controller to the endpoint
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
